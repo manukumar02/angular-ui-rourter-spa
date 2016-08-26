7bbba5a0502e1c3f6b8939bf969f391e140bbe9a
Manu Sharma <manukumar02@gmail.com>
2016-08-27 00:41:25 +0530

    Config initialized and controller

--- js/app.config.js
+++ js/app.config.js
@@ -1,5 +1,13 @@
 ﻿(function () {
     'use strict';
 
-    
+    angular.module('App').config(routeConfig);
+    routeConfig.$inject = ['$stateProvider'];
+
+    function routeConfig($stateProvider) {
+    	$stateProvider.state('contactList', {
+    		url: '/contacts',
+    		controller: 'contactList'
+    	})
+    }
 })();
--- js/controllers/contact-list.js
+++ js/controllers/contact-list.js
@@ -1,5 +1,23 @@
 ﻿(function () {
     'use strict';
 
+    angular.module('App').controller('contactList', contactList);
+    contactList.$inject = ['$scope', 'contacts'];
 
+    function contactList($scope, contacts) {
+    	$scope.contactlist = null;
+
+    	contacts.getContacts().then(function(response) {
+    		var tmp = [];
+
+    		response.data.contacts.forEach(function(item) {
+    			var initial = item.name.slice(0, 1);
+
+    			if(tmp.indexOf(initial) === -1) {
+    				tmp.push(initial);
+    			}
+    		});
+    		$scope.contactlist = tmp;
+    	});
+    }
 })();
