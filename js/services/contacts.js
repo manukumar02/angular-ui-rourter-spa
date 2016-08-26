372d00795043cd1691612de1a21ec02c02344c6c
Manu Sharma <manukumar02@gmail.com>
2016-08-26 23:57:11 +0530

    Initializing Angular UI-Router- App - Example

---
+++ js/services/contacts.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app').factory('contacts', contacts);
+
+    contacts.$inject = ['$http'];
+
+    function contacts($http) {
+        return {
+            getContacts: function () {
+                return $http.get('/contactsStub.json');
+            }
+        };
+    }
+}());
