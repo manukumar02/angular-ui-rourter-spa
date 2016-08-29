6da2205a2d7d1e65ce70fc37f61007e44acc09e0
Manu Sharma <manukumar02@gmail.com>
2016-08-29 22:28:48 +0530

    config update

--- js/app.config.js
+++ js/app.config.js
@@ -8,7 +8,13 @@
         $stateProvider.state('contactList', {
             url: '/contacts',
             controller: 'contactList',
-            templateUrl: './views/contactList.html'
+            templateUrl: './views/contactList.html',
+            resolve: {
+                delay: function ($timeout) {
+                    return $timeout(function () {}, 2000)
+                }
+            }
+
         }).state('contactLetter', {
             url: '/contacts/:letter',
             controller: 'contactLetter',
